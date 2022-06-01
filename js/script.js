b9cd8925ba6486eb88425b1107adf52d68881b37
hectororigel1994 <horigel@byui.edu>
2022-06-01 06:25:30 -0500

    COmmit

--- js/script.js
+++ js/script.js
@@ -326,3 +326,41 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+/*Smooth Scrolling*/
+// Select all links with hashes
+$('a[href*="#body"], [href*="#about"], [href*="#services"], [href*="#portfolio"], [href*="#our-team"], [href*="#pricing"], [href*="#blog"], [href*="#contact"]')
+  // Remove links that don't actually link to anything
+  .not('[href=""]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
